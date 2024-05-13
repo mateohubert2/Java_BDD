8fa5f1581cfd43777f9ca0a69ad1a46edcddd9f7
mateohubert2 <mathub2003@gmail.com>
2024-05-13 17:56:20 +0200

    Commit secondaire

--- td-java-bdd/src/App.java
+++ td-java-bdd/src/App.java
@@ -6,26 +6,9 @@ import java.sql.SQLException;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            System.out.println(e.getMessage());
-        }
-        try {
-            Connection myConnection = DriverManager.getConnection(
-              "jdbc:mysql://localhost:3307/poly_sports",
-              "mateo" ,
-              "esirem"
-            );
-            Statement myStatement = myConnection.createStatement();
-            ResultSet results = myStatement.executeQuery("SELECT `name`, `required_participants` FROM `sport`;");
-            while(results.next()){
-                final String name = results.getString("name");
-                final int number = results.getInt("required_participants");
-                System.out.println("Pour le sport: " + name + ", il faut: " + number + " participants");
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
+        MYSQLDatabase mysql = new MYSQLDatabase("localhost", 3307, "poly_sports", "mateo", "esirem");
+        mysql.loadDriver();
+        mysql.connect();
+        //mysql.createStatement();
     }
 }
---
+++ td-java-bdd/src/MYSQLDatabase.java
@@ -0,0 +1,54 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class MYSQLDatabase {
+    private String host;
+    private int port;
+    private String databaseName;
+    private String user;
+    private String password;
+    private Connection connection;
+    private static boolean driverLoaded;
+    MYSQLDatabase(String host, int port, String databaseName, String user, String password){
+        this.host = host;
+        this.port = port;
+        this.databaseName = databaseName;
+        this.user = user;
+        this.password = password;
+        connection = null;
+        driverLoaded = false;
+    }
+    public void connect(){
+        try {
+            connection = DriverManager.getConnection(
+                "jdbc:mysql://"+host+":"+port+"/" + databaseName,
+                user ,
+                password
+                );
+            connection.createStatement();
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+    public Statement createStatement(){
+        Statement statement = null;
+        try {
+            statement = connection.createStatement();
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        return statement;
+    }
+    public static void loadDriver(){
+        if(driverLoaded != true){
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+            } catch (ClassNotFoundException e) {
+                System.err.println(e.getMessage());
+            }
+            driverLoaded = true;
+        }
+    }
+}
