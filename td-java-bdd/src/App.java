f3fdbf1ada59910d63e1c72824c8ecdea0fde860
mateohubert2 <mathub2003@gmail.com>
2024-05-14 11:27:21 +0200

    SportsDAO fonctionnelle

--- td-java-bdd/src/App.java
+++ td-java-bdd/src/App.java
@@ -1,4 +1,5 @@
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -8,7 +9,10 @@ public class App {
     public static void main(String[] args) throws Exception {
         PolySportsDatabase poly_sport = PolySportsDatabase.getInstance();
         poly_sport.connect();
-        Sport sport = new Sport(2, "natation", 1);
-        System.out.println("Pour faire le sport: " + sport.getName()+ ", il faut: " + sport.getRequiredParticipants() + " participant et c'est le sport: " + sport.getId());
+        SportsDAO dao = new SportsDAO(poly_sport);
+        ArrayList<Sport> sports = dao.findAll();
+        for(int i = 0; i < sports.size(); i++){
+            System.out.println("Pour le sport: " + sports.get(i).getName() + " il faut: " + sports.get(i).getRequiredParticipants() + " participants et c'est le sport ayant pour id: " + sports.get(i).getId());
+        }
     }
 }
---
+++ td-java-bdd/src/SportsDAO.java
@@ -0,0 +1,29 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class SportsDAO {
+    private MYSQLDatabase database;
+    
+    public SportsDAO(MYSQLDatabase database){
+        this.database = database;
+    }
+    public ArrayList<Sport> findAll(){
+        ArrayList<Sport> sports = new ArrayList<>();
+        Statement myStatement = database.createStatement();
+        try {
+            ResultSet myResults = myStatement.executeQuery("SELECT `name`, `id`, `required_participants` FROM `sport`;");
+            while(myResults.next()){
+                int id = myResults.getInt("id");
+                String name = myResults.getString("name");
+                int required_participants = myResults.getInt("required_participants");
+                Sport sport = new Sport(id, name, required_participants);
+                sports.add(sport);
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        return sports;
+    }
+}
