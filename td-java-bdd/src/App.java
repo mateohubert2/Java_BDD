4012a04c6b61c0e09f22aedaf0bf2fa596d1b323
mateohubert2 <mathub2003@gmail.com>
2024-05-17 16:27:12 +0200

    Test de Id existant et inexistant

--- td-java-bdd/src/App.java
+++ td-java-bdd/src/App.java
@@ -12,9 +12,16 @@ public class App {
         SportsDAO dao = new SportsDAO(poly_sport);
         ArrayList<Sport> sports = dao.findAll();
         Sport sport = dao.findById(1);
+        Sport sport_inexistant = dao.findById(15);
         for(int i = 0; i < sports.size(); i++){
             System.out.println("Pour le sport: " + sports.get(i).getName() + " il faut: " + sports.get(i).getRequiredParticipants() + " participants et c'est le sport ayant pour id: " + sports.get(i).getId());
         }
         System.out.println("Pour le sport: " + sport.getName() + " il faut: " + sport.getRequiredParticipants() + " participants et c'est le sport ayant pour id: " + sport.getId());
+
+        try {
+            System.out.println("Pour le sport: " + sport_inexistant.getName() + " il faut: " + sport_inexistant.getRequiredParticipants() + " participants et c'est le sport ayant pour id: " + sport_inexistant.getId());
+        } catch (Exception e) {
+            System.err.println("Le sport demandé n'existe pas");
+        }
     }
 }
